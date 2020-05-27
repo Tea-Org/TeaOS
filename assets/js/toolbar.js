343f10750eee08db2c0619058e43e8cab20dbf21
Kellis <34947108+Gumbraise@users.noreply.github.com>
2020-05-27 16:35:00 +0200

    Added the battery level

--- assets/js/toolbar.js
+++ assets/js/toolbar.js
@@ -1,4 +1,9 @@
 var hour = document.getElementsByClassName('hour')[0];
+var bat = document.getElementsByClassName('battery');
+var font = document.getElementsByClassName('font')[0];
+var sec_font = document.getElementsByClassName('secfont')[0];
+var button = document.getElementsByClassName('taskb');
+
 
 window.onload = function hourK() {
     var ladate=new Date()
@@ -9,3 +14,62 @@ window.onload = function hourK() {
     hour.innerHTML = h+":"+m
     setTimeout(hourK, 1000)
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+    let l = 0
+    const task_box = document.getElementsByClassName('task_box')[0];
+
+	button[1].addEventListener("click", () => {
+        if (l == 0) {
+            task_box.style.display = "block";
+            l = 1
+        } else {
+            task_box.style.display = "none";
+            l = 0
+        }
+	});
+});
+
+// Battery script:
+navigator.getBattery().then(function(battery) {
+    function updateAllBatteryInfo(){
+      updateChargeInfo();
+      updateLevelInfo();
+    }
+    updateAllBatteryInfo();
+  
+    battery.addEventListener('chargingchange', function(){
+      updateChargeInfo();
+    });
+    function updateChargeInfo(){
+        bat[0].innerHTML = '<i class="fas fa-plug"></i>'
+        bat[1].innerHTML = '<i class="fas fa-plug"></i>'
+        sec_font.innerHTML = 'In charge'
+    }
+  
+    battery.addEventListener('levelchange', function(){
+      updateLevelInfo();
+    });
+    function updateLevelInfo(){
+        var level = battery.level * 100
+        if (level < 10) {
+            bat[0].innerHTML = '<i class="fas fa-battery-empty"></i>'
+            bat[1].innerHTML = '<i class="fas fa-battery-empty"></i>'
+        } else if (level > 9 && level < 30) {
+            bat[0].innerHTML = '<i class="fas fa-battery-quarter"></i>'
+            bat[1].innerHTML = '<i class="fas fa-battery-quarter"></i>'
+        } else if (level > 29 && level < 70) {
+            bat[0].innerHTML = '<i class="fas fa-battery-half"></i>'
+            bat[1].innerHTML = '<i class="fas fa-battery-half"></i>'
+        } else if (level > 69 && level < 90) {
+            bat[0].innerHTML = '<i class="fas fa-battery-three-quarter"></i>'
+            bat[1].innerHTML = '<i class="fas fa-battery-three-quarter"></i>'
+        } else {
+            bat[0].innerHTML = '<i class="fas fa-battery-full"></i>'
+            bat[1].innerHTML = '<i class="fas fa-battery-full"></i>'
+        }
+        font.innerHTML = battery.level * 100 + "%"
+        sec_font.innerHTML = "On battery"
+    }
+  
+});

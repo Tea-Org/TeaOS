919f14052513dfb9f354e4853e85f73b9c053726
Kellis <34947108+Gumbraise@users.noreply.github.com>
2020-05-29 23:32:11 +0200

    Added login style page

--- assets/js/toolbar.js
+++ assets/js/toolbar.js
@@ -3,6 +3,7 @@ var bat = document.getElementsByClassName('battery');
 var font = document.getElementsByClassName('font')[0];
 var sec_font = document.getElementsByClassName('secfont')[0];
 var button = document.getElementsByClassName('taskb');
+var box = document.getElementsByClassName('box');
 
 
 window.onload = function hourK() {
@@ -19,16 +20,34 @@ window.addEventListener("DOMContentLoaded", () => {
     let l = 0
     const task_box = document.getElementsByClassName('task_box')[0];
 
-	button[1].addEventListener("click", () => {
-        if (l == 0) {
-            task_box.style.display = "block";
-            l = 1
-        } else {
-            task_box.style.display = "none";
-            l = 0
-        }
-	});
+    for (let i = 0; i < 10; i++) {
+        button[i].addEventListener("click", () => {
+            if (l == 0) {
+                task_box.style.display = "block";
+                box[i].style.display = "block";
+                l = 1
+            } else {
+                task_box.style.display = "none";
+                box[i].style.display = "none";
+                l = 0
+            }
+        });
+    }
 });
+
+/*if (navigator.onLine === true) {
+    alert('You are online');
+} else {
+    alert('You are offline');
+}
+
+window.addEventListener('online', function() {
+	alert('Current status : online');
+});
+
+window.addEventListener('offline', function() {
+	alert('Current status : offline');
+});*/
 
 // Battery script:
 navigator.getBattery().then(function(battery) {

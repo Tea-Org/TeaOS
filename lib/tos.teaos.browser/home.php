c74d740983e96a84bb46acd3885bb5a7f475240c
Kellis <34947108+Gumbraise@users.noreply.github.com>
2020-07-02 00:33:57 +0200

    ✅ Added ENTER touch in Browser

---
+++ lib/tos.teaos.browser/home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel="stylesheet" href="assets/css/home.css">
+    <script src="assets/js/main.js"></script>
+
+</head>
+<body>
+    <div class="body">
+        <div class="item">
+            <div class="square"><img src="https://api.faviconkit.com/google.com/1024" alt=""></div>
+            <p>YouTube</p>
+        </div>
+        <div class="item">
+            <div class="square"><img src="https://api.faviconkit.com/kellis.fr/1024" alt=""></div>
+            <p>YouTube</p>
+        </div>
+        <div class="item">
+            <div class="square"><img src="https://api.faviconkit.com/google.com/1024" alt=""></div>
+            <p>YouTube</p>
+        </div>
+        <div class="item">
+            <div class="square"><img src="https://api.faviconkit.com/youtube.com/1024" alt=""></div>
+            <p>YouTube</p>
+        </div>
+    </div>
+
+</body>
+</html>

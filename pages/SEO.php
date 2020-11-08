98f12107aeca70fe395e3b745232d92729c8343f
Gumbraise <vikiclashonyt@gmail.com>
2020-11-08 20:21:32 +0100

    🔍 Improve SEO

--- pages/SEO.php
+++ pages/SEO.php
@@ -12,14 +12,17 @@ echo '<!DOCTYPE html>
     <meta property="og:title" content="TeaOS · Free Online Operating System For All" />
     <meta property="og:description" content="Free fast and secure OS for everyone, suitable complements to any OS with different Desktop Environments">
     <meta property="og:type" content="article" />
-    <meta property="og:url" content="https://teaos.kellis.fr/" />
+    <meta property="og:url" content="https://teaos.software/" />
+    <meta property="og:image" content="https://teaos.software/etc/img/seo.png">
     <meta property="fb:app_id" content="2296148857110995">
-    <meta property="ia:markup_url" content="https://teaos.kellis.fr/">
+    <meta property="ia:markup_url" content="https://teaos.software/">
     <meta property="ia:markup_url_dev" content="https://kellis.fr/?e=TeaOS">
     <meta property="ia:rules_url" content="https://kellis.fr/?e=IA">
     <meta property="ia:rules_url_dev" content="https://kellis.fr/?e=IA">
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="https://teaos.kellis.fr/" />
+    <meta name="twitter:image" content="https://teaos.software/etc/img/seo.png">
+    <meta name="twitter:image_alt" content="TeaOS · Free Online Operating System For All">
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="https://teaos.software/" />
     <meta name="twitter:title" content="TeaOS · Free Online Operating System For All" />
     <meta name="twitter:description" content="Free fast and secure OS for everyone, suitable complements to any OS with different Desktop Environments" />
 ';
